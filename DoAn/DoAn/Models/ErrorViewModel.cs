14dd23434d01a968be3653714828f23dcec96dde
Theanh1301 <125528549+Theanh1301@users.noreply.github.com>
2023-05-02 20:59:14 +0700

    Delete DoAn directory

--- DoAn/DoAn/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace DoAn.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
